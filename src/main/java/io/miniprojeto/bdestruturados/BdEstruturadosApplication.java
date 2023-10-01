28e0a53ada1cf83233e2edaec757efd8c04bf1f9
J3ffn <jefferson.mangueira12@gmail.com>
2023-10-01 13:26:31 -0300

    add: lógica XML

--- src/main/java/io/miniprojeto/bdestruturados/BdEstruturadosApplication.java
+++ src/main/java/io/miniprojeto/bdestruturados/BdEstruturadosApplication.java
@@ -1,13 +1,17 @@
 package io.miniprojeto.bdestruturados;
 
-import org.springframework.boot.SpringApplication;
+import io.miniprojeto.bdestruturados.model.Municipio;
+import io.miniprojeto.bdestruturados.service.XmlService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 @SpringBootApplication
 public class BdEstruturadosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BdEstruturadosApplication.class, args);
+		XmlService<Municipio> xmlService = new XmlService<>();
+
 	}
 
 }
