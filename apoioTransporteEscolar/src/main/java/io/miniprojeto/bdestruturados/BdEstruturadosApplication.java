36f71583f6cd04ccb8dab4fe5b3e9d48f1762567
J3ffn <jefferson.mangueira12@gmail.com>
2023-10-01 14:36:45 -0300

    add git ignore e dependência no POM

--- apoioTransporteEscolar/src/main/java/io/miniprojeto/bdestruturados/BdEstruturadosApplication.java
+++ apoioTransporteEscolar/src/main/java/io/miniprojeto/bdestruturados/BdEstruturadosApplication.java
@@ -5,17 +5,19 @@ import io.miniprojeto.bdestruturados.service.XmlService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
+import java.io.IOException;
 
 @SpringBootApplication
 public class BdEstruturadosApplication {
 
 	private static final XmlService<Municipio> xmlService = new XmlService<>();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 //		xmlService.xmlToEntity("resources.bd.PNATEAlunosAtendidos.xml", Municipio.class);
 
-		File file = new File("resources.bd.PNATEAlunosAtendidos.xml");
-		System.out.println(file.getName());
+		File file = new File("src/main/resources/bd/PNATEAlunosAtendidos.xml");
+
+		System.out.println(file.exists());
 
 	}
 
